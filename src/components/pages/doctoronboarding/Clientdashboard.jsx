fa539f749c4bf631e0d3608afb11ba33228dac6c
Abhay Jangir <abhayjangir65@gmail.com>
2026-05-21 11:35:21 +0530

    updated code for the authentication

--- src/components/pages/doctoronboarding/Clientdashboard.jsx
+++ src/components/pages/doctoronboarding/Clientdashboard.jsx
@@ -708,48 +708,14 @@ export default function ClientDashboard() {
       });
   }
 
-  function getCookie(name) {
-    let cookieValue = null;
-
-    if (document.cookie && document.cookie !== "") {
-
-      const cookies = document.cookie.split(";");
-
-      for (let i = 0; i < cookies.length; i++) {
-
-        const cookie = cookies[i].trim();
-
-        if (cookie.substring(0, name.length + 1) === (name + "=")) {
-
-          cookieValue = decodeURIComponent(
-            cookie.substring(name.length + 1)
-          );
-
-          break;
-        }
-      }
-    }
-
-    return cookieValue;
-  }
-
   function triggerMessage(client) {
-
-    const csrftoken =
-      document.cookie
-        .split("; ")
-        .find(row => row.startsWith("csrftoken="))
-        ?.split("=")[1];
 
     fetch(`${BASE_URL}/send-message/`, {
 
       method: "POST",
 
-      credentials: "include",
-
       headers: {
         "Content-Type": "application/json",
-        "X-CSRFToken": csrftoken,
       },
 
       body: JSON.stringify({
